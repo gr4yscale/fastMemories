4577f655e066fd39b9b235e3b9d9c92bcc483edc
Tyler Powers <gr4yscale@gmail.com>
2015-10-01 22:22:21 -0700

    sloppy integration of Adobe's XMP framework

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,27 +1,38 @@
 #include "ofApp.h"
 
+static FILE * sLogFile = stdout;
+
+static XMP_Status DumpCallback ( void * refCon, XMP_StringPtr outStr, XMP_StringLen outLen )
+{
+    XMP_Status	status	= 0;
+    size_t		count;
+    FILE *		outFile	= static_cast < FILE * > ( refCon );
+    
+    count = fwrite ( outStr, 1, outLen, outFile );
+    if ( count != outLen ) status = errno;
+    return status;
+    
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    ofSetLogLevel(OF_LOG_VERBOSE);
     ofBackground(0, 0, 0);
     ofSetFrameRate(60);
     
     statusFont.loadFont("Ubuntu Mono derivative Powerline Bold.ttf", 32);
     indexFont.loadFont("Ubuntu Mono derivative Powerline Bold.ttf", 200);
     
-    // recurse subdirectories?
+    // recurse subphotosDirectories?
     
-    string photosDirectory = "/Users/gr4yscale/Desktop/[fastMemoriesTest]";
+    loadImagesMetadata();
+    
+// load table of random colors for testing without images
+//    for (int i = 0; i < 3000; i++) {
+//        colors.push_back(ofColor(ofRandom(255),ofRandom(255), ofRandom(255)));
+//    }
 
-    dir.open(photosDirectory);
-    dir.allowExt("jpg");
-    dir.allowExt("JPG");
-    dir.listDir();
-    
-    // load table of random colors for testing without images
-    for (int i = 0; i < 3000; i++) {
-        colors.push_back(ofColor(ofRandom(255),ofRandom(255), ofRandom(255)));
-    }
 }
 
 //--------------------------------------------------------------
@@ -31,39 +42,207 @@ void ofApp::update(){
     
     preciseFileIndex += frameChangeDelta;
     
-    if (preciseFileIndex >= colors.size()) {
-        preciseFileIndex = colors.size();
+    if (preciseFileIndex >= photosDir.size() - 1) {
+        preciseFileIndex = photosDir.size() - 1;
     } else if (preciseFileIndex <= 0) {
         preciseFileIndex = 0;
     }
     
-    //        updateTexture();
+//    updateTexture();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-    ofColor color = colors[currentFileIndex()];
-    ofBackground(color);
+//    ofColor color = colors[currentFileIndex()];
+//    ofBackground(color);
+    
+    ofPushMatrix();
+        ofTranslate((ofGetWidth() - texture.getWidth()) / 2.0, 0);
+        texture.draw(0, 0);
+    ofPopMatrix();
+    
     drawGUI();
 }
+
+void ofApp::loadDirectory(ofDirectory dir) {
+    
+    int size = dir.listDir();
+    for(int i = 0; i < size; i++) {
+        if (dir.getFile(i).isDirectory() == 1) {
+            ofDirectory subDir(dir.getFile(i).getAbsolutePath());
+            loadDirectory(subDir);
+        } else {
+            fileNames.push_back(dir.getPath(i));
+        }
+    }
+}
+
+
+void ofApp::loadImagesMetadata() {
+
+    string originalDir = "/Users/gr4yscale/[fastMemoriesTest]/source";
+    string exportedDir = "/Users/gr4yscale/[fastMemoriesTest]/exported";
+    
+    photosDir.allowExt("jpg");
+    photosDir.allowExt("JPG");
+
+    loadDirectory(exportedDir);
+    
+    if(!SXMPMeta::Initialize()){
+        ofLogError() << "Could not initialize XMP toolkit!";
+        return;
+    } else{
+        ofLogVerbose() << "Initialised XMP toolkit";
+    }
+    
+    XMP_OptionBits options = 0;
+    
+    // Must initialize SXMPFiles before we use it
+    if(!SXMPFiles::Initialize(options)){
+        ofLogError() << "Could not initialize XMP SXMPFiles.";
+        return;
+    } else{
+        ofLogVerbose() << "Initialised XMP SXMPFiles";
+    }
+    
+    // load xmp files and sort photos array by XMP
+    
+    for(int i = 0; i < fileNames.size(); i++) {
+        string path = fileNames[i];
+        size_t lastSlashIndex = path.find_last_of("/");
+        string subPath = path.substr(exportedDir.length() + 1, lastSlashIndex - exportedDir.length() - 1);
+        string originalFileName = path.substr(lastSlashIndex + 18, path.length() - lastSlashIndex);
+        string sourceXMPFileName = originalFileName.replace(originalFileName.end() - 3, originalFileName.end(), "xmp");
+        string xmpFileNameFullPath = originalDir + "/" + subPath + "/" + sourceXMPFileName;
+        
+        loadXMPTest(xmpFileNameFullPath.c_str());
+        break;
+    }
+}
+
+void ofApp::loadXMPTest(const char * fileName) {
+    bool ok;
+    char buffer [1000];
+    
+    SXMPMeta  xmpMeta;
+    SXMPFiles xmpFile;
+    XMP_FileFormat format;
+    XMP_OptionBits openFlags, handlerFlags;
+    XMP_PacketInfo xmpPacket;
+    
+    cout << "derpin";
+    cout << fileName;
+    
+    sprintf ( buffer, "Dumping main XMP for %s", fileName );
+//    WriteMinorLabel ( sLogFile, buffer );
+    
+    xmpFile.OpenFile ( fileName, kXMP_UnknownFile, kXMPFiles_OpenForRead );
+    ok = xmpFile.GetFileInfo ( 0, &openFlags, &format, &handlerFlags );
+    if ( ! ok ) return;
+    
+    fprintf ( sLogFile, "File info : format = \"%.4s\", handler flags = %.8X\n", &format, handlerFlags );
+    fflush ( sLogFile );
+    
+    cout << " goin for it";
+    
+    ok = xmpFile.GetXMP ( &xmpMeta, 0, &xmpPacket );
+    if ( ! ok ) return;
+    
+    
+    cout << "here now";
+    
+    XMP_Int32 offset = (XMP_Int32)xmpPacket.offset;
+    XMP_Int32 length = xmpPacket.length;
+    fprintf ( sLogFile, "Packet info : offset = %d, length = %d\n", offset, length );
+    fflush ( sLogFile );
+    
+    fprintf ( sLogFile, "\nInitial XMP from %s\n", fileName );
+    xmpMeta.DumpObject ( DumpCallback, sLogFile );
+    
+    xmpFile.CloseFile();
+}
+
+void ofApp::loadXMP(string path){
+    
+    ofLogVerbose() << "Attempting to load XMP from: " << path;
+    
+    try {
+        // Options to open the file with - read only and use a file handler
+        XMP_OptionBits opts = kXMPFiles_OpenForRead | kXMPFiles_OpenUseSmartHandler;
+        
+        bool ok;
+        
+        SXMPMeta::Initialize();
+        
+        SXMPFiles myFile;
+        std::string status = "";
+        
+        // First we try and open the file
+        ok = myFile.OpenFile(path, kXMP_UnknownFile, opts);
+        if (!ok) {
+            status += "No smart handler available for " + path + "\n";
+            status += "Trying packet scanning.\n";
+            
+            // Now try using packet scanning
+            opts = kXMPFiles_OpenForRead | kXMPFiles_OpenUsePacketScanning;
+            ok = myFile.OpenFile(path, kXMP_UnknownFile, opts);
+        }
+        
+        // If the file is open then read the metadata
+        if (ok) {
+            ofLogVerbose() << status;
+            ofLogVerbose() << path << " is opened successfully";
+            
+            // Create the xmp object and get the xmp data
+            SXMPMeta meta;
+            if (myFile.GetXMP(&meta, NULL, NULL)) {
+                cout << "yup";
+            } else {
+                cout << "nah";
+            }
+
+//            string labelValue = "";
+//            string labelPath = "Label";
+//            meta.GetProperty(kXMP_NS_XMP, labelPath.c_str(), &labelValue, NULL);
+//            cout << labelValue;
+            
+//            meta.DumpObject(dumpXMPObject, NULL);
+            
+            SXMPIterator iter(meta, kXMP_NS_RDF);
+            string schemaNS="", propPath="", propVal="";
+            while(iter.Next(&schemaNS, &propPath, &propVal)){
+                cout << propPath << " = " << propVal << endl;
+            }
+            
+        } else {
+            ofLogError() << "Unable to open " << path;
+        }
+
+        myFile.CloseFile();
+        
+    } catch(XMP_Error & e) {
+        ofLogError() << "ERROR: " << e.GetErrMsg();
+    }
+}
+
+
 
 void ofApp::drawGUI() {
 
     ofSetColor(255, 255, 255, 255);
     
-//    statusFont.drawString(ofToString(dir.getName(currentFileIndex())), 10, 40);
-    indexFont.drawString(ofToString(currentFileIndex()), ofGetWidth() / 2.0 - 160.0, ofGetHeight() / 2.0);
-    
-    ofSetColor(255, 255, 255, 100);
+//    statusFont.drawString(ofToString(photosDir.getName(currentFileIndex())), 10, 40);
+//    indexFont.drawString(ofToString(currentFileIndex()), ofGetWidth() / 2.0 - 160.0, ofGetHeight() / 2.0);
+//    ofSetColor(255, 255, 255, 100);
     
     float markerWidth = (400.0 * frameChangeDelta);
-    ofRect(ofGetWidth() / 2.0, ofGetHeight() - 40.0, markerWidth, 20);
+    ofRect(ofGetWidth() / 2.0, ofGetHeight() - 20.0, markerWidth, 8);
 }
 
 void ofApp::updateTexture() {
 
-    ofLoadImage(texture, dir.getPath(currentFileIndex()));
+    ofLoadImage(texture, photosDir.getPath(currentFileIndex()));
 }
 
 long ofApp::currentFileIndex() {
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,14 +2,41 @@
 
 #include "ofMain.h"
 
+// Must be defined to instantiate template classes
+#define TXMP_STRING_TYPE std::string
+
+// Must be defined to give access to XMPFiles
+#define XMP_INCLUDE_XMPFILES 1
+
+#ifdef TARGET_OSX
+#define MAC_ENV 1
+#endif
+
+// Ensure XMP templates are instantiated
+#include "XMP.incl_cpp"
+
+// Provide access to the API
+#include "XMP.hpp"
+
+struct Photo
+{
+    string fileName;
+    double time_taken;
+    string dateTimeTakenString;
+};
+
 class ofApp : public ofBaseApp{
 
     private:
+        void loadDirectory(ofDirectory dir);
+        void loadXMP(string path);
         bool currentFileIndexWithinBounds();
         void updateTexture();
+        void loadImagesMetadata();
         void drawGUI();
         long currentFileIndex();
-
+        void loadXMPTest(const char * fileName);
+    
 	public:
 		void setup();
 		void update();
@@ -30,13 +57,15 @@ class ofApp : public ofBaseApp{
         bool updatingFrameRate = 0;
         float frameChangeDelta;
         double preciseFileIndex = 0;
-    
+
         ofImage img;
         ofTexture texture;
     
-        ofDirectory dir;
+        ofDirectory photosDir;
 		ofTrueTypeFont statusFont;
         ofTrueTypeFont indexFont;
  
         vector<ofColor> colors;
+        vector<Photo> photos;
+        vector<string> fileNames;
 };
